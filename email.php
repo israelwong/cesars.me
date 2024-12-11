27f01dcbd9dfa195a2c856dcdf594ea760d1b66c
Israel Wong <israelwong@iMac-de-Israel.local>
2024-12-11 15:33:10 -0600

    update email utf8

--- email.php
+++ email.php
@@ -1,11 +1,11 @@
 <?php
 
-$nombre=$_GET['nombre'];
-$telefono=$_GET['telefono'];
-$email=$_GET['email'];
-$msg=$_GET['msg'];
+$nombre = $_GET['nombre'];
+$telefono = $_GET['telefono'];
+$email = $_GET['email'];
+$msg = $_GET['msg'];
 
-if($email=="") return;
+if ($email == "") return;
 
 $asunto = "WEB Cesars";
 
@@ -14,19 +14,17 @@ $headers = "MIME-Version: 1.0\r\nContent-type: text/html; charset=iso-8859-1\r\n
 $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 
 //dirección del remitente
-$headers .= "From: CSS <".$email.">\r\n";
-        
-//direcci—n de respuesta, si queremos que sea distinta que la del remitente
-$headers .= "Reply-To: ".$email." \r\n";
+$headers .= "From: CSS <" . $email . ">\r\n";
 
-$salida='';
-$salida.= '<p>Nombre: '.utf8_decode($nombre).'<p>';
-$salida.= '<p>Tel.: '.utf8_decode($telefono).'<p>';
-$salida.= '<p>Correo electrónico.: '.utf8_decode($email).'<p>';
-$salida.= '<p>Mensaje: '.utf8_decode($msg).'<br>';
+//direcci—n de respuesta, si queremos que sea distinta que la del remitente
+$headers .= "Reply-To: " . $email . " \r\n";
+
+$salida = '';
+$salida .= '<p>Nombre: ' . mb_convert_encoding($nombre, 'ISO-8859-1', 'UTF-8') . '<p>';
+$salida .= '<p>Tel.: ' . mb_convert_encoding($telefono, 'ISO-8859-1', 'UTF-8') . '<p>';
+$salida .= '<p>Correo electrónico.: ' . mb_convert_encoding($email, 'ISO-8859-1', 'UTF-8') . '<p>';
+$salida .= '<p>Mensaje: ' . mb_convert_encoding($msg, 'ISO-8859-1', 'UTF-8') . '<br>';
 
 //mail('ing.israel.wong@gmail.com',$asunto,$salida,$headers);
-mail('cesaraseprof@hotmail.com',$asunto,$salida,$headers);
+mail('cesaraseprof@hotmail.com', $asunto, $salida, $headers);
 //echo true;
-
-?>
