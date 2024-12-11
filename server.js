b4970f711cc7d21d271294650e54b413497dd737
Israel Wong <israelwong@iMac-de-Israel.local>
2024-12-11 17:29:18 -0600

    update api

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sendMail = require('./pages/api/sendmail');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.post('/api/sendmail', sendMail);
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
